0b50526d3525ee0a07d07ae075ceafec9b8eb5d2
Oldberamot <daune.arnaud@gmail.com>
2021-10-28 09:02:46 +0200

    initial commit

---
+++ src/main/java/be/bruxellesformation/bf_projet_final/BfProjetFinalApplication.java
@@ -0,0 +1,13 @@
+package be.bruxellesformation.bf_projet_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfProjetFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BfProjetFinalApplication.class, args);
+    }
+
+}
