62d658e82f99da77fa3040a63a4abe7b003ce1a9
Oldberamot <daune.arnaud@gmail.com>
2021-10-28 09:52:19 +0200

    add application.yml

--- src/main/java/be/bruxellesformation/bf_projet_final/BfProjetFinalApplication.java
+++ src/main/java/be/bruxellesformation/bf_projet_final/BfProjetFinalApplication.java
@@ -2,12 +2,14 @@ package be.bruxellesformation.bf_projet_final;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BfProjetFinalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BfProjetFinalApplication.class, args);
+
     }
 
 }
